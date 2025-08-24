d9307b00e943cce7d3c2e572bea82ad989940607
Rahil Shah <shahrahil1705@gmail.com>
2025-08-23 23:52:44 -0400

    NASA's random image generate repository

---
+++ index.js
@@ -0,0 +1,49 @@
+import express from "express"
+import ejs from "ejs"
+import axios from "axios"
+
+const app = express();
+const port = 4000;
+const API_URL = "https://api.nasa.gov/planetary/apod";
+const API_KEY = "uzthIKkFviTruqdfCgLN5wPpPcHaMgmPyh55LH1d"
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+
+app.get("/", async (req,res)=>{
+    // (API_URL+"?api_key="+API_KEY);
+    // const data = request.data;
+    // console.log(data); 
+    res.render("index.ejs");
+    // console.log(req.body);
+});
+
+app.get("/get-images", async (req,res)=>{
+    // (API_URL+"?api_key="+API_KEY);
+    // const data = request.data;
+    // console.log(data); 
+    res.render("index.ejs");
+    // console.log(req.body);
+});
+
+app.post("/get-images", async (req, res)=>{
+    const numImages = req.body.numberOfImages;
+    try{
+        const imageURL = API_URL+"?api_key="+API_KEY+"&count="+numImages;
+        const request = await axios.get(imageURL);
+        const bodyData = request.data;
+        // console.log(bodyData);
+        res.render("index.ejs",{
+            content: bodyData
+        });
+    }
+    catch(error){
+        res.sendStatus(501);
+        console.error(error);
+    }
+})
+
+app.listen(port, ()=>{
+    console.log(`App started at port ${port}`);
+});
